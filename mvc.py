3ba5de6535c52e62e7e36cc249032f3a57910b27
Abtin <abtin.kit.shirvani@gmail.com>
2025-03-10 23:44:21 -0400

    Added get_data() func

---
+++ mvc.py
@@ -0,0 +1,22 @@
+import requests
+from io import StringIO
+import pandas as pd
+
+
+def get_data():
+    """Returns the MVC dataset from open data Toronto"""
+    base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
+    url = base_url + "/api/3/action/package_show"
+    params = {
+        "id": ("motor-vehicle-collisions-involving-killed-or-"
+               "seriously-injured-persons")
+        }
+    package = requests.get(url, params=params).json()
+
+    resource = package['result']['resources'][0]
+    url = base_url + "/datastore/dump/" + resource["id"]
+    resource_dump_data = requests.get(url).text
+    return StringIO(resource_dump_data)
+
+
+df = pd.read_csv(get_data())
