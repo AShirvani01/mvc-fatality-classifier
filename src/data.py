10c0a98f8cf08756663f71c95243cf48699223d9
Abtin <abtin.kit.shirvani@gmail.com>
2025-05-19 21:17:15 -0400

    Refactored download_streets_data and rename_files functions

--- src/data.py
+++ src/data.py
@@ -19,12 +19,12 @@ def get_collision_data() -> pd.DataFrame:
         column corresponds to a feature.
     """
     base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
-    url = f"{base_url}/api/3/action/package_show"
+    package_url = f"{base_url}/api/3/action/package_show"
     params = {
         "id": ("motor-vehicle-collisions-involving-killed-or-"
                "seriously-injured-persons")
     }
-    package = requests.get(url, params=params).json()
+    package = requests.get(package_url, params=params).json()
 
     resource = package['result']['resources'][0]
     url = f"{base_url}/datastore/dump/{resource['id']}"
@@ -74,35 +74,19 @@ def load_external_data(file_path: Path, crs="EPSG:4326") -> gpd.GeoDataFrame:
     return gdf
 
 
-def rename_files(path: Path, new_file_name: str) -> None:
-
-    if not path.exists():
-        raise FileNotFoundError("File or subdirectory doesn't exist. Check for"
-                                " typos and if files have been downloaded.")
-
-    if path.is_file():
-        path.rename(path.parent / new_file_name)
-
-    if path.is_dir():
-        for file in path.iterdir():
-            if file.is_file():
-                file.rename(path / f"{new_file_name}{file.suffix}")
-
-
 def download_streets_data(output_dir: Path = DATA_DIR / "canada_streets") -> None:
     url = (
         "https://www12.statcan.gc.ca/census-recensement/2011"
         "/geo/RNF-FRR/files-fichiers/lrnf000r24a_e.zip"
     )
-    response = requests.get(url, stream=True)
+    response = requests.get(url)
 
-    output_dir.mkdir(exist_ok=True)
-
-    with response as r:
-        with ZipFile(BytesIO(r.content)) as zip_file:
-            zip_file.extractall(output_dir)
-
-    rename_files(output_dir, 'canada_streets')
+    with ZipFile(BytesIO(response.content)) as zip_file:
+        zip_file.extractall(output_dir)
+    
+        for file in zip_file.namelist():
+            file_path = output_dir / file
+            file_path.rename(output_dir / f'canada_streets{file_path.suffix}')
 
 
 def download_hospital_data(output_dir: Path = DATA_DIR) -> None:
