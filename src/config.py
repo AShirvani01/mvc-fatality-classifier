d342996ffc2a55ca41f30e818b5c682098a1f05e
Abtin <abtin.kit.shirvani@gmail.com>
2025-05-19 21:31:51 -0400

    Updated NEIGHBOURHOODS_PATH

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ def get_project_root():
     return Path(__file__).parent.parent.resolve()
 
 
-data_dir = get_project_root() / 'data'
+DATA_DIR = get_project_root() / 'data'
 
-HEALTH_SERVICES_PATH = data_dir / 'ontario_health_services.geojson'
-NEIGHBOURHOODS_PATH = data_dir / 'toronto_neighbourhoods' / 'neighbourhoods.shp'
-ROADS_PATH = data_dir / 'canada_roads' / 'canada_roads.shp'
+HEALTH_SERVICES_PATH = DATA_DIR / 'ontario_health_services.geojson'
+NEIGHBOURHOODS_PATH = DATA_DIR / 'toronto_neighbourhoods.geojson'
+STREETS_PATH = DATA_DIR / 'canada_streets' / 'canada_streets.shp'
