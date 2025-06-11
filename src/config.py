c467ac581eddc85417f083c0b1681f4244996b93
Abtin <abtin.kit.shirvani@gmail.com>
2025-06-11 14:51:45 -0400

    Removed validate_range_order, added XGBParams

--- src/config.py
+++ src/config.py
@@ -48,22 +48,12 @@ class CBParams(BaseModel):
     objective: Objective | list[Objective] = 'Logloss'
     eval_metric: EvalMetric | list[EvalMetric] = 'Accuracy'
     auto_class_weights: AutoClassWeights | list[AutoClassWeights] = 'Balanced'
-    depth: Depth | tuple[Depth, Depth] = (1, 16)
-    l2_leaf_reg: L2LeafReg | tuple[L2LeafReg, L2LeafReg] = (0., 15.)
-    learning_rate: LearningRate | tuple[LearningRate, LearningRate] = (0.01, 0.1)
-    rsm: RSM | tuple[RSM, RSM] = (0.1, 1)
+    depth: Depth | tuple[Depth, Depth] | list[Depth] = (1, 16)
+    l2_leaf_reg: L2LeafReg | tuple[L2LeafReg, L2LeafReg] | list[L2LeafReg] = (100., 0.)
+    learning_rate: LearningRate | tuple[LearningRate, LearningRate] | list[LearningRate] = (0.01, 0.1)
+    rsm: RSM | tuple[RSM, RSM] | list[RSM] = (0.1, 1)
 
     model_config = ConfigDict(extra='forbid')
-
-    # Validation
-    @field_validator('depth', 'l2_leaf_reg', 'learning_rate', 'rsm')
-    @classmethod
-    def validate_range_order(cls, field):
-        if type(field) is tuple:
-            if field[0] > field[1]:
-                raise ValueError(f'Min {field} must be <= Max {field}.')
-        return field
-
 
 
 class CBModelConfig(BaseModel):
@@ -71,7 +61,7 @@ class CBModelConfig(BaseModel):
 
     Args:
         early_stopping_round (int): If provided, stop training if one
-            metric of one validation data doesn't improve in the last 
+            metric of one validation data doesn't improve in the last
             `early_stopping_round` rounds. Defaults to 100.
         fold_count (int): Number of different cross validation folds to train.
             Defaults to 5.
@@ -84,6 +74,39 @@ class CBModelConfig(BaseModel):
     fold_count: int = 5
     num_boost_round: int = 10_000
     seed: int = 42
-    verbose: Union[bool, int] = 100
+    verbose: Union[bool, int] = False
+
+    model_config = ConfigDict(extra='forbid')
+
+
+class XGBParams(BaseModel):
+    """XGBoost model training parameters. For details, see
+    [XGBoost's documentation](
+    https://xgboost.readthedocs.io/en/stable/parameter.html).
+
+    Args:
+        objective (Literal): Loss function in training. Defaults to Logloss.
+        eval_metric (Literal): Metric to compare models. Defaults to Accuracy.
+        max_depth (int): Limit the max depth for tree model.
+        reg_lambda (float): Coefficient of the L2 regularization term.
+        learning_rate (float): Learning rate.
+        subsample (float): Random subspace method. The percentage of features
+            to use at each split selection.
+    """
+    # Type alias
+    Objective: ClassVar[type] = Literal['binary:logistic']
+    EvalMetric: ClassVar[type] = Literal['logloss', 'error', 'auc']
+    MaxDepth: ClassVar[type] = Annotated[int, Field(ge=1, le=16)]
+    RegLambda: ClassVar[type] = NonNegativeFloat
+    LearningRate: ClassVar[type] = Annotated[float, Field(gt=0, le=1)]
+    SubSample: ClassVar[type] = Annotated[float, Field(gt=0, le=1)]
+
+    # Params/Defining defaults
+    objective: Objective | list[Objective] = 'Logloss'
+    eval_metric: EvalMetric | list[EvalMetric] = 'Accuracy'
+    max_depth: MaxDepth | tuple[MaxDepth, MaxDepth] | list[MaxDepth] = (1, 16)
+    reg_lambda: RegLambda | tuple[RegLambda, RegLambda] | list[RegLambda] = (0., 100.)
+    learning_rate: LearningRate | tuple[LearningRate, LearningRate] | list[LearningRate] = (0.01, 0.1)
+    subsample: SubSample | tuple[SubSample, SubSample] | list[SubSample] = (0.1, 1)
 
     model_config = ConfigDict(extra='forbid')
